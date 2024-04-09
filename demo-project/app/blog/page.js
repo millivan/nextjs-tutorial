4d435b51db7afc274bfadc4a85a6d5a79388b43e
millivan <limzyu02@gmail.com>
2024-04-09 14:04:22 +0800

    Reorganize file structure and basic introduction to routes and dynamic routes in NextJS

---
+++ demo-project/app/blog/page.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+const BlogPage = () => {
+  return (
+    <main>
+      <h1>The Blog</h1>
+      <p>
+        <Link href="/blog/post-1">Blog 1</Link>
+      </p>
+      <p>
+        <Link href="/blog/post-2">Blog 2</Link>
+      </p>
+    </main>
+  );
+};
+
+export default BlogPage;
